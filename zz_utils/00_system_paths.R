115c777477dd282887ae13fc9eb728524d87a232
Cristian Mejia <cristianmejia00@hotmail.com>
2025-01-15 09:48:17 +0900

    update system paths

--- zz_utils/00_system_paths.R
+++ zz_utils/00_system_paths.R
@@ -1,12 +1,12 @@
 # Find system and root
 if (Sys.info()["sysname"] == "Windows") {
   print("You are running R on a Windows machine")
-  raw_data_folder_path <- "C:\\Users\\crist\\OneDrive\\Documentos"
-  bibliometrics_folder_path <- ""
+  input_folder_path <- "C:\\Users\\crist\\OneDrive\\Documentos\\input"
+  output_folder_path <- "C:\\Users\\crist\\OneDrive\\Documentos\\output"
 } else if (Sys.info()["sysname"] == "Darwin") {
   print("You are running R on a Mac machine")
-  raw_data_folder_path <- "/Users/cristian/Library/CloudStorage/OneDrive-Personal/Documentos"
-  bibliometrics_folder_path <- "/Users/cristian/Library/CloudStorage/GoogleDrive-cristianmejia00@gmail.com/My Drive/Bibliometrics_Drive"
+  input_folder_path <- "/Users/cristian/Library/CloudStorage/OneDrive-Personal/Documentos"
+  output_folder_path <- "/Users/cristian/Library/CloudStorage/GoogleDrive-cristianmejia00@gmail.com/My Drive/Bibliometrics_Drive"
 } else {
   print("You are running R on a different operating system")
 }
